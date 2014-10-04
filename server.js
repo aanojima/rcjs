64c8b993999ed149cf19c0b88df200bb72ce61c1
Aaron Nojima <aanojima@mit.edu>
2014-10-04 16:24:53 -0400

    Socket Cross-domain test

--- server.js
+++ server.js
@@ -3,10 +3,11 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
+var routes = require('./routes');
 
 var app = module.exports = express.createServer();
+var io = require('socket.io')(app);
 
 // Configuration
 
@@ -29,9 +30,22 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+// Socket.IO
+
 // Routes
 
 app.get('/', routes.index);
+
+// Create App
+// app.post('/application', application.create);
+// app.postI'/controller', controller.create);
+io.on('connection', function(socket){
+  console.log("New connection");
+  socket.on('echo', function(data){
+    console.log('echoing ' + data + ' back to client');
+    io.emit('echo', data);
+  });
+});
 
 var port = process.env.OPENSHIFT_NODEJS_PORT;
 var ip = process.env.OPENSHIFT_NODEJS_IP;
