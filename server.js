279486d8779bbc8df52879469df92317be5ba61c
Aaron Nojima <aanojima@mit.edu>
2014-10-04 22:47:17 -0400

    Allow Control Access Origin *

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var io = module.exports.io = require('socket.io')(app);
 // Configuration
 
 app.configure(function(){
-	app.use(express.cookieParser());
+	app.use(
+    express.cookieParser());
     app.use(express.session({secret: '1234567890QWERTY'}));
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
@@ -20,6 +21,9 @@ app.configure(function(){
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
+    app.use(function(req,res,next){
+      res.header('Access-Control-Allow-Origin', '*');
+    });
 });
 
 app.configure('development', function(){
