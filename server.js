9c54ce6a22a16dc588c126c8d314b92f8771bb5c
Aaron Nojima <aanojima@mit.edu>
2014-10-04 22:49:57 -0400

    Fix Access Control

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.configure(function(){
     app.use(express.static(__dirname + '/public'));
     app.use(function(req,res,next){
       res.header('Access-Control-Allow-Origin', '*');
+      next();
     });
 });
 
